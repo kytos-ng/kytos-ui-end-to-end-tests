c66d434e5e60e27348730afa0efd820fbe0a5e9f
usmanazFIU <muhaziz@fiu.edu>
2026-02-16 16:30:10 -0600

    filters

--- tests/test_statusmenu_001_dashboard.py
+++ tests/test_statusmenu_001_dashboard.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from tests.utils.statusmenu_page import StatusmenuPage
 
@@ -5,11 +7,11 @@ from tests.utils.statusmenu_page import StatusmenuPage
 @pytest.mark.parametrize("api_url", [("API_MEFELINE_URL", "http://localhost:18181/api/kytos/mef_eline/v2/evc/")],
                          indirect=True)
 class TestStatusMenu:
-    """Positive test cases for successful EVC creation"""
+    """Positive test cases for successful status checks"""
 
     @pytest.fixture(autouse=True)
     def setup_method(self, driver, base_url, api_url, default_timeout):
-        """Initialize EVCPage object before each test."""
+        """Initialize Status menu object before each test."""
         self.statusmenu_page = StatusmenuPage(driver, base_url, api_url, default_timeout)
 
     def test_001_check_switches(self):
@@ -17,7 +19,10 @@ class TestStatusMenu:
         # Navigate to status menu
         assert self.statusmenu_page.navigate_to_statusmenu(), "Failed to navigate to Pathfinder form"
 
-        # check number of switches,links and interfaces
+        # check number of switches,links and interfaces and filters
         assert self.statusmenu_page.check_switches(), "switches data not consistent"
+        assert self.statusmenu_page.check_switches_filters(), "switch filters not working"
         assert self.statusmenu_page.check_links(), "links data not consistent"
+        assert self.statusmenu_page.check_links_filters(), "links filters not working"
         assert self.statusmenu_page.check_interfaces(), "interfaces data not consistent"
+        assert self.statusmenu_page.check_interfaces_filters(), "interfaces filters not working"
--- tests/utils/statusmenu_page.py
+++ tests/utils/statusmenu_page.py
@@ -11,30 +11,12 @@ from selenium.webdriver.common.keys import Keys
 
 class StatusmenuPage:
     """
-    Page Object Model for the EVC creation and management page in Kytos UI.
+    Page Object Model for the Status menu page in Kytos UI.
     Encapsulates all UI interactions and locators.
     """
     SELECTORS = {
         # Navigation
-        'statusmenu_button': (By.CSS_SELECTOR, 'button[data-test="main-button"][title="Status Menu"]'),
-
-        # Form fields
-        'source_input': (By.XPATH, "//*[@id='source']/div/div/div/input"),
-        'destination_input': (By.XPATH, "//*[@id='destination']/div/div/div/input"),
-        'paths': (By.XPATH,"//*[@id='app']/section[2]/div[1]/div/button[1]"),
-        'path_table': (By.CLASS_NAME,"k-property-panel"),
-        'bandwidth': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[5]/div[3]/input"),
-        'reliability': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[6]/div[3]/input"),
-        'delay': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[7]/div[3]/input"),
-
-        # Optional fields
-        'utilization': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[8]/div[3]/input"),
-        'priority': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[9]/div[3]/input"),
-        'spf_max_paths': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[14]/div/div/input"),
-        'spf_max_path_cost': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[15]/div/div/input"),
-
-        # Buttons
-        'submit_button': (By.XPATH, "//*[@id='app']/div[1]/div/div[7]/div/div/div/div/div[16]/button")
+        'statusmenu_button': (By.CSS_SELECTOR, 'button[data-test="main-button"][title="Status Menu"]')
     }
 
     def __init__(self, driver: WebDriver, base_url: str, api_url: str, default_timeout: int):
@@ -50,7 +32,7 @@ class StatusmenuPage:
         return self.wait.until(EC.presence_of_element_located((by, value)))
 
     def navigate_to_statusmenu(self):
-        """Navigate from homepage to EVC creation form."""
+        """Navigate from homepage to Status menu form."""
         print("Navigating to Status Menu...")
         self.driver.get(self.base_url)
 
@@ -87,6 +69,76 @@ class StatusmenuPage:
         if num_switches_ui==num_switches_api:
             return True
 
+    def check_switches_filters(self):
+        switchName_Filter=self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/table/thead/tr[2]/th[1]/input")
+        switchName_Filter.click()
+        filter_test_value=os.getenv('switch_filter_value')
+        switchName_Filter.send_keys(filter_test_value)
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='switch_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_switches_filter = len(rows)
+        name_flag=0
+        if num_switches_filter==0:
+            name_flag=1
+        else:
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 6, f"Expected 6 tds, found {len(cells)}"
+                print(cells[1].text)
+                if cells[1].text==filter_test_value:
+                    name_flag=1
+
+        for _ in range(len(filter_test_value)):
+            switchName_Filter.send_keys(Keys.BACKSPACE)
+
+        switchStatus_Filter=self.driver.find_element(By.XPATH, "//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/table/thead/tr[2]/th[2]/input")
+        switchStatus_Filter.click()
+        switchStatus_Filter.send_keys('up')
+
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='switch_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_switches_filter = len(rows)
+        status_flag = 0
+        if num_switches_filter == 0:
+            status_flag = 1
+        else:
+            status_flag = 1
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 6, f"Expected 6 tds, found {len(cells)}"
+                print(cells[2].text)
+                if cells[2].text != 'UP':
+                    status_flag = 0
+
+        for _ in range(2):
+            switchStatus_Filter.send_keys(Keys.BACKSPACE)
+
+        switchEnabled_Filter=self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/table/thead/tr[2]/th[4]/input")
+        switchEnabled_Filter.click()
+        switchEnabled_Filter.send_keys('true')
+
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='switch_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_switches_filter = len(rows)
+        enabled_flag = 0
+        if num_switches_filter == 0:
+            enabled_flag = 1
+        else:
+            enabled_flag = 1
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 6, f"Expected 6 tds, found {len(cells)}"
+                print(cells[4].text)
+                if cells[4].text != 'true':
+                    enabled_flag = 0
+
+        switch_table=self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/label")
+        switch_table.click()
+        time.sleep(2)
+
+        if name_flag==1 and status_flag==1 and enabled_flag==1:
+            return True
+
     def check_links(self):
         table = self.driver.find_element(By.XPATH, "//table[@data-test='link_table']")
         rows = table.find_elements(By.XPATH, ".//tbody/tr")
@@ -107,7 +159,75 @@ class StatusmenuPage:
         if num_links_ui == num_links_api:
             return True
 
+    def check_links_filters(self):
+        linkName_Filter = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/table/thead/tr[2]/th[1]/input")
+        linkName_Filter.click()
+        filter_test_value = os.getenv('link_filter_value')
+        linkName_Filter.send_keys(filter_test_value)
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='link_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_links_filter = len(rows)
+        name_flag = 0
+        if num_links_filter == 0:
+            name_flag = 1
+        else:
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 6, f"Expected 6 tds, found {len(cells)}"
+                print(cells[1].text)
+                if cells[1].text == filter_test_value:
+                    name_flag = 1
 
+        for _ in range(len(filter_test_value)):
+            linkName_Filter.send_keys(Keys.BACKSPACE)
+
+        linkStatus_Filter = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/table/thead/tr[2]/th[2]/input")
+        linkStatus_Filter.click()
+        linkStatus_Filter.send_keys('up')
+
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='link_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_links_filter = len(rows)
+        status_flag = 0
+        if num_links_filter == 0:
+            status_flag = 1
+        else:
+            status_flag = 1
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 6, f"Expected 6 tds, found {len(cells)}"
+                print(cells[2].text)
+                if cells[2].text != 'UP':
+                    status_flag = 0
+
+        for _ in range(2):
+            linkStatus_Filter.send_keys(Keys.BACKSPACE)
+
+        linkEnabled_Filter = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/table/thead/tr[2]/th[4]/input")
+        linkEnabled_Filter.click()
+        linkEnabled_Filter.send_keys('true')
+
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='link_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_links_filter = len(rows)
+        enabled_flag = 0
+        if num_links_filter == 0:
+            enabled_flag = 1
+        else:
+            enabled_flag = 1
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 6, f"Expected 6 tds, found {len(cells)}"
+                print(cells[4].text)
+                if cells[4].text != 'true':
+                    enabled_flag = 0
+
+        link_table = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/label")
+        link_table.click()
+        time.sleep(2)
+
+        if name_flag == 1 and status_flag == 1 and enabled_flag == 1:
+            return True
     def check_interfaces(self):
         table = self.driver.find_element(By.XPATH, "//table[@data-test='interface_table']")
         rows = table.find_elements(By.XPATH, ".//tbody/tr")
@@ -126,4 +246,74 @@ class StatusmenuPage:
         print(f"Number of interfaces: {num_interfaces_api}")
 
         if num_interfaces_ui == num_interfaces_api:
+            return True
+
+    def check_interfaces_filters(self):
+        interfaceNode_Filter = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/table/thead/tr[2]/th[1]/input")
+        interfaceNode_Filter.click()
+        filter_test_value = os.getenv('interface_filter_value')
+        interfaceNode_Filter.send_keys(filter_test_value)
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='interface_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_interface_filter = len(rows)
+        name_flag = 0
+        if num_interface_filter == 0:
+            name_flag = 1
+        else:
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 7, f"Expected 7 tds, found {len(cells)}"
+                print(cells[1].text)
+                if cells[1].text == filter_test_value:
+                    name_flag = 1
+
+        for _ in range(len(filter_test_value)):
+            interfaceNode_Filter.send_keys(Keys.BACKSPACE)
+
+        interfaceStatus_Filter = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/table/thead/tr[2]/th[3]/input")
+        interfaceStatus_Filter.click()
+        interfaceStatus_Filter.send_keys('up')
+
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='interface_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_interface_filter = len(rows)
+        status_flag = 0
+        if num_interface_filter == 0:
+            status_flag = 1
+        else:
+            status_flag = 1
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 7, f"Expected 7 tds, found {len(cells)}"
+                print(cells[3].text)
+                if cells[3].text != 'UP':
+                    status_flag = 0
+
+        for _ in range(2):
+            interfaceStatus_Filter.send_keys(Keys.BACKSPACE)
+
+        interfaceEnabled_Filter = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[3]/div/div[2]/table/thead/tr[2]/th[5]/input")
+        interfaceEnabled_Filter.click()
+        interfaceEnabled_Filter.send_keys('true')
+
+        table = self.driver.find_element(By.XPATH, "//table[@data-test='interface_table']")
+        rows = table.find_elements(By.XPATH, ".//tbody/tr")
+        num_interface_filter = len(rows)
+        enabled_flag = 0
+        if num_interface_filter == 0:
+            enabled_flag = 1
+        else:
+            enabled_flag = 1
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                assert len(cells) == 7, f"Expected 7 tds, found {len(cells)}"
+                print(cells[5].text)
+                if cells[5].text != 'true':
+                    enabled_flag = 0
+
+        interface_table = self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[3]/label")
+        interface_table.click()
+        time.sleep(2)
+
+        if name_flag == 1 and status_flag == 1 and enabled_flag == 1:
             return True
